9100bb1d7e6ebb35e8f3a7eb3cddcface53ff91f
Arnol Plazas <plazasarnol@gmail.com>
2023-01-10 07:04:04 -0500

    I generated and validated a token with pyjwt.

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from jwt_manager import create_token
 
 app = FastAPI()
 app.title = 'Movies'
 app.version = '0.0.1'
 
+class User(BaseModel):
+    email: str
+    password: str
 
 class Movie(BaseModel):
     id: Optional[int] = None
@@ -51,15 +55,24 @@ movies = [
 def message():
     return HTMLResponse('<h1> Hello world</h1>')
 
+@app.post('/login', tags=['auth'])
+def login(user: User):
+    if user.email == 'arnol@mail.com' and user.password == '1234':
+        token: str = create_token(user.dict())
+        return JSONResponse(status_code=200, content=token)
 
-@app.get('/movies', tags=['movies'], response_model=List[Movie])
+
+@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
 def get_movies() -> List[Movie] :
-    return JSONResponse(content=movies)
+    return JSONResponse(status_code=200, content=movies)
 
 @app.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     movie = list(filter(lambda x: x['id'] == id, movies))
-    return JSONResponse(content=movie)
+    if len(movie) != 0:
+        return JSONResponse(content=movie)
+    else:
+        return JSONResponse(status_code=404, content=[])
 
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
@@ -67,12 +80,12 @@ def get_movie_by_category(category: str = Query(min_length=5, max_length=15)) ->
     movie_by_category = list(filter(lambda x: x['category'] == category, movies))
     return JSONResponse(content=movie_by_category)
 
-@app.post('/movies', tags=['movies'], response_model=dict)
+@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
 def create_movie(movie: Movie) -> dict:
     movies.append(movie)
-    return JSONResponse(content={'message': 'a movie has been added'})
+    return JSONResponse(status_code=201, content={'message': 'a movie has been added'})
 
-@app.put('/movies/{id}', tags=['movies'], response_model=dict)
+@app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def update_movie(id: int, movie: Movie) -> dict:
     for item in movies:
         if item['id'] == id:
@@ -81,12 +94,12 @@ def update_movie(id: int, movie: Movie) -> dict:
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.category
-    return JSONResponse(content={'message': 'a movie has been modified'})
+    return JSONResponse(status_code=200, content={'message': 'a movie has been modified'})
 
 
-@app.delete('/movies/{id}', tags=['movies'], response_model=dict)
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def delete_movie(id: int) -> dict:
     for item in movies:
         if item['id'] == id:
             movies.remove(item)
-    return JSONResponse(content={'message': 'a movie has been deleted'})
+    return JSONResponse(status_code=200, content={'message': 'a movie has been deleted'})
